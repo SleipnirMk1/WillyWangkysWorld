f1cc1f4194b57287a0043734274aecf8dafbd898
raffifpr <68497910+raffifpr@users.noreply.github.com>
2020-11-24 21:00:31 +0700

    Update main.c

--- Fungsi atau Prosedur/main.c
+++ Fungsi atau Prosedur/main.c
@@ -1,8 +1,30 @@
+#include jam.c
+
+JAM Current_Time;
+
 void main(Stack S)
 {
 	while (!IsStackEmpty(S))
 	{
 		Pop(S)); /*Kosongkan stack*/
 	}
-	execute(S);
+	printf ('// Tidak mengeksekusi perintah dari stack //\n');
+	printf ('Main phase day ', P.day, '\n');
+	CetakMap;
+	printf ('Legend: \n');
+	printf ('A = Antrian\n')
+	printf ('P = Player\n')
+	printf ('W = Wahana\n')
+	printf ('O = Office\n')
+	printf ('<, ^, >, v = Gerbang\n')
+		
+	printf ('Nama : '); printf(P.nama);
+	printf ('Money : '); printf(P.money);
+	Current_Time = MakeJAM(09,00,00);
+	printf ('Current Time : '); TulisJAM(Current_Time); printf('\n');
+	Next_Time = MakeJAM(21,00,00);
+	printf ('Closing Time : '); TulisJAM(Next_Time); printf('\n');
+	Time_Remaining = Durasi(Current_Time, Next_Time);
+	printf ('Time Remaining: '); CetakDurasi(Time_Remaining); printf('\n');
+	CetakAntrian();
 }
