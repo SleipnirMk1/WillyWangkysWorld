11f93a30e49924773a34478c284e78dab15ad1f2
raffifpr <68497910+raffifpr@users.noreply.github.com>
2020-11-28 00:02:26 +0700

    Update gerak.c
    revisi isWall, gerak, dan parameter

--- Fungsi atau Prosedur/gerak.c
+++ Fungsi atau Prosedur/gerak.c
@@ -1,6 +1,6 @@
-boolean IsWall(float X, float Y)
+boolean IsWall(float X, float Y, int A)
 {
-	return (X == 0)|(X == 19)|(Y == 19)|(Y == 0);
+	return ((X == 0)|(X == 19)|(Y == 19)|(Y == 0))&&(!IsGerbang(X, Y, A));
 }
 boolean IsGerbang(float X, float Y, int A)
 {
@@ -25,111 +25,98 @@ void gerak(char X, P)
 {
 	if (X == 0)
 	{
-		if (!IsWall(Absis(P),Ordinat(P) + 1))
+		if (!IsWall(Absis(P),Ordinat(P) + 1, Area(P)))
 		{
-			if(IsGerbang(Absis(P),Ordinat(P) + 1, Area(P)))
+			Geser (P, 0, 1, 0);
+		}
+		else if(IsGerbang(Absis(P),Ordinat(P) + 1, Area(P)))
+		{
+		/*Point dengan Ordinat == 0*/
+			if (Area(P) == 3)
 			{
-				/*Point dengan Ordinat == 0*/
-				if (Area(P) == 3)
-				{
-					Absis(P) = 9;
-					Ordinat(P) = 8;
-					Area(P) = 1;
-				}
-				else if (Area(P) == 4)
-				{
-					
-					Absis(P) = 9;
-					Ordinat(P) = 8;
-					Area(P) = 2;
-				}
+				Absis(P) = 9;
+				Ordinat(P) = 8;
+				Area(P) = 1;
 			}
-			else
+			else if (Area(P) == 4)
 			{
-				Geser (P, 0, 1, 0);
+				
+				Absis(P) = 9;
+				Ordinat(P) = 8;
+				Area(P) = 2;
 			}
 		}
+
 	}
 	else if (X == 1)
 	{
-		if (!IsWall(Absis(P)-1,Ordinat(P)))
+		if (!IsWall(Absis(P)-1,Ordinat(P), Area(P)))
 		{
-			if(IsGerbang(Absis(P),Ordinat(P) + 1, Area(P)))
+			Geser (P, -1, 0, 0);
+		}
+		else if(IsGerbang(Absis(P),Ordinat(P) + 1, Area(P)))
+		{
+			/*Point dengan Absis == 0*/
+			if (Area(P) == 2)
 			{
-				/*Point dengan Absis == 0*/
-				if (Area(P) == 2)
-				{
-					Absis(P) = 18;
-					Ordinat(P) = 4;
-					Area(P) = 1;
-				}
-				else if (Area(P) == 4)
-				{
-					
-					Absis(P) = 18;
-					Ordinat(P) = 4;
-					Area(P) = 3;
-				}
+				Absis(P) = 18;
+				Ordinat(P) = 4;
+				Area(P) = 1;
 			}
-			else
-			{
-				Geser (P, -1, 0, 0);
+			else if (Area(P) == 4)
+			{					
+				Absis(P) = 18;
+				Ordinat(P) = 4;
+				Area(P) = 3;
 			}
 		}
+		
 	}
 	else if (X == 2)
 	{
-		if (!IsWall(Absis(P),Ordinat(P) - 1))
+		if (!IsWall(Absis(P),Ordinat(P) - 1, Area(P)))
 		{
-			if(IsGerbang(Absis(P),Ordinat(P) + 1, Area(P)))
-			{
-				/*Point dengan Ordinat == 9*/
-				if (Area(P) == 2)
-				{
-					Absis(P) = 9;
-					Ordinat(P) = 1;
-					Area(P) = 4;
-				}
-				else if (Area(P) == 1)
-				{
-					
-					Absis(P) = 9;
-					Ordinat(P) = 1;
-					Area(P) = 3;
-				}
-			}
-			else
-			{
 				Geser (P, 0, -1, 0);
+		}
+		else if(IsGerbang(Absis(P),Ordinat(P) + 1, Area(P)))
+		{
+			/*Point dengan Ordinat == 9*/
+			if (Area(P) == 2)
+			{
+				Absis(P) = 9;
+				Ordinat(P) = 1;
+				Area(P) = 4;
+			}
+			else if (Area(P) == 1)
+			{
+				
+				Absis(P) = 9;
+				Ordinat(P) = 1;
+				Area(P) = 3;
 			}
 		}
 	}
 	else if (X == 3)
 	{
-		if (!IsWall(Absis(P) + 1,Ordinat(P)))
+		if (!IsWall(Absis(P) + 1,Ordinat(P), Area(P)))
 		{
-			if(IsGerbang(Absis(P),Ordinat(P) + 1, Area)))
-			{
-				/*Point dengan Absis == 19*/
-				if (Area(P) == 1)
-				{
-					Absis(P) = 1;
-					Ordinat(P) = 4;
-					Area(P) = 2;
-				}
-				else if (Area(P) == 3)
-				{
-					
-					Absis(P) = 1;
-					Ordinat(P) = 4;
-					Area(P) = 4;
-				}
-			}
-			else
-			{
-				Geser (P, 1, 0, 0);
-			}
+			Geser (P, 1, 0, 0);
 		}
-		
+		if(IsGerbang(Absis(P),Ordinat(P) + 1, Area)))
+		{
+			/*Point dengan Absis == 19*/
+			if (Area(P) == 1)
+			{
+				Absis(P) = 1;
+				Ordinat(P) = 4;					
+				Area(P) = 2;
+			}
+			else if (Area(P) == 3)
+			{
+				Absis(P) = 1;
+				Ordinat(P) = 4;
+				Area(P) = 4;
+			}
+		}	
 	}
 }
